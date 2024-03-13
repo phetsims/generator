41b8aad8b81f37833b335710c58e8f585de0c6f9
pixelzoom <cmalley@pixelzoom.com>
2024-03-13 17:13:45 -0600

    factor out FELSim, https://github.com/phetsims/faradays-electromagnetic-lab/issues/101

--- js/generator-main.ts
+++ js/generator-main.ts
@@ -6,41 +6,17 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import Sim, { SimOptions } from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import GeneratorStrings from './GeneratorStrings.js';
-import PreferencesModel from '../../joist/js/preferences/PreferencesModel.js';
-import FELPreferencesNode from '../../faradays-electromagnetic-lab/js/common/view/preferences/FELPreferencesNode.js';
-import FELConstants from '../../faradays-electromagnetic-lab/js/common/FELConstants.js';
 import GeneratorScreen from '../../faradays-electromagnetic-lab/js/generator/GeneratorScreen.js';
+import FELSim from '../../faradays-electromagnetic-lab/js/common/FELSim.js';
 
 simLauncher.launch( () => {
-
   const titleStringProperty = GeneratorStrings.generator.titleStringProperty;
-
   const screens = [
     new GeneratorScreen( Tandem.ROOT.createTandem( 'generatorScreen' ) )
   ];
-
-  const options: SimOptions = {
-    credits: FELConstants.CREDITS,
-    phetioDesigned: true,
-    preferencesModel: new PreferencesModel( {
-      visualOptions: {
-        supportsProjectorMode: true
-      },
-      simulationOptions: {
-        customPreferences: [ {
-          createContent: tandem => new FELPreferencesNode( {
-            hasEarthFeature: false,
-            tandem: tandem.createTandem( 'simPreferences' )
-          } )
-        } ]
-      }
-    } )
-  };
-
-  const sim = new Sim( titleStringProperty, screens, options );
+  const sim = new FELSim( titleStringProperty, screens, false /* hasEarthFeature */ );
   sim.start();
 } );
